f7a31a61bbd6c5a91473188b26a828362368f855
John Evans <john@wearelighthouse.com>
2024-08-22 14:47:53 +0100

    More WIP glowy building test stuff

--- src/js/tile.js
+++ src/js/tile.js
@@ -47,10 +47,16 @@ export class Tile {
     this.svgElement = createSvgElement();
     this.svgElement.style.width = `${cellCssSize}vmin`;
     this.svgElement.style.height = `${cellCssSize}vmin`;
+    this.svgElement.setAttribute('viewBox', '0 0 4 4');
+
+    const roadElement = createSvgElement('path');
+    roadElement.setAttribute('stroke', '#43ab');
+    roadElement.setAttribute('d', 'm 0 3 h 4');
+    this.svgElement.append(roadElement);
 
     this.element.append(this.svgElement);
 
-    const buildingCssWidth = 8;
+    const buildingCssWidth = 7;
     const buildingCssHeight = 17;
     const buildingScale = 1;
 
@@ -59,7 +65,7 @@ export class Tile {
     this.building1.classList.add('cube');
     this.building1.style.width = `${buildingCssWidth}vmin`;
     this.building1.style.height = `${buildingCssWidth}vmin`;
-    this.building1.style.background = '#291676';
+    this.building1.style.background = '#43a';
     this.building1.style.transform = `translateX(-5vmin) translateY(-5vmin) translateZ(${buildingCssHeight}vmin)`; // Raise up by height of building
     this.building1.sides = [
       createElement(),
@@ -79,48 +85,45 @@ export class Tile {
     this.building1.sides[2].style.background = `
       linear-gradient(
         #0000 calc(50% - ${buildingScale * 2}vmin),
-        #0002 50%,
+        #0004 50%,
         #0000 calc(50% + ${buildingScale * 2}vmin)
       ),
-      linear-gradient(90deg,
-        #216 ${buildingScale * 1}vmin,
-        #0000 0 ${buildingCssWidth - 1}vmin,
-        #216 0
+      repeating-linear-gradient(90deg,
+        #328 0 ${buildingScale * 1}vmin,
+        #0000 0 ${(buildingScale + 1) * 1}vmin
       ),
       linear-gradient(
-        #216 ${buildingScale * 1}vmin,
+        #328 ${buildingScale * 1}vmin,
         #4de 0 ${buildingScale * 2}vmin,
-        #216 0 ${buildingScale * 3}vmin,
+        #328 0 ${buildingScale * 3}vmin,
         #4de 0 ${buildingScale * 4}vmin,
-        #216 0 ${buildingScale * 5}vmin,
+        #328 0 ${buildingScale * 5}vmin,
         #4de 0 ${buildingScale * 6}vmin,
-        #216 0 ${buildingScale * 7}vmin,
+        #328 0 ${buildingScale * 7}vmin,
         #4de 0 ${buildingScale * 8}vmin,
-        #216 0 ${buildingScale * 9}vmin,
+        #328 0 ${buildingScale * 9}vmin,
         #56f 0 ${buildingScale * 10}vmin,
-        #216 0 ${buildingScale * 11}vmin,
+        #328 0 ${buildingScale * 11}vmin,
         #56f 0 ${buildingScale * 12}vmin,
-        #216 0 ${buildingScale * 13}vmin,
+        #328 0 ${buildingScale * 13}vmin,
         #b2f 0 ${buildingScale * 14}vmin,
-        #216 0 ${buildingScale * 15}vmin,
-        #b2f 0 ${buildingScale * 16}vmin,
-        #216 0 ${buildingScale * 18}vmin,
-        #b2f 0 ${buildingScale * 19}vmin,
-        #216 0 ${buildingScale * 20}vmin,
+        #328 0 ${buildingScale * 15}vmin,
+
+        #328 0 ${buildingScale * 20}vmin,
         #b2f 0 ${buildingScale * 21}vmin,
-        #216 0 ${buildingScale * 22}vmin,
+        #328 0 ${buildingScale * 22}vmin,
         #56f 0 ${buildingScale * 23}vmin,
-        #216 0 ${buildingScale * 24}vmin,
+        #328 0 ${buildingScale * 24}vmin,
         #56f 0 ${buildingScale * 25}vmin,
-        #216 0 ${buildingScale * 26}vmin,
+        #328 0 ${buildingScale * 26}vmin,
         #4de 0 ${buildingScale * 27}vmin,
-        #216 0 ${buildingScale * 28}vmin,
+        #328 0 ${buildingScale * 28}vmin,
         #4de 0 ${buildingScale * 29}vmin,
-        #216 0 ${buildingScale * 30}vmin,
+        #328 0 ${buildingScale * 30}vmin,
         #4de 0 ${buildingScale * 31}vmin,
-        #216 0 ${buildingScale * 32}vmin,
+        #328 0 ${buildingScale * 32}vmin,
         #4de 0 ${buildingScale * 33}vmin,
-        #216 0
+        #328 0
       )
     `;
     this.building1.sides[2].style.mask = 'linear-gradient(#000 50%, #0000)';
@@ -132,51 +135,56 @@ export class Tile {
     `);
     this.building1.sides[2].append(blurryness1);
     this.building1.sides[3].style.transform = `rotateX(-90deg) translateY(${buildingCssHeight}vmin) translateZ(${buildingCssWidth / 2}vmin)`;
+
+
+    // linear-gradient(90deg,
+    //   #215 ${buildingScale * 1}vmin,
+    //   #0000 0 ${buildingCssWidth - 1}vmin,
+    //   #215 0
+    // ),
+
     this.building1.sides[3].style.background = `
       linear-gradient(
         #0000 calc(50% - ${buildingScale * 2}vmin),
-        #0002 50%,
+        #0004 50%,
         #0000 calc(50% + ${buildingScale * 2}vmin)
       ),
-      linear-gradient(90deg,
-        #104 ${buildingScale * 1}vmin,
-        #0000 0 ${buildingCssWidth - 1}vmin,
-        #104 0
+      repeating-linear-gradient(90deg,
+        #215 0 ${buildingScale * 1}vmin,
+        #0000 0 ${(buildingScale + 1) * 1}vmin
       ),
       linear-gradient(
-        #104 ${buildingScale * 1}vmin,
+        #215 ${buildingScale * 1}vmin,
         #4de 0 ${buildingScale * 2}vmin,
-        #104 0 ${buildingScale * 3}vmin,
+        #215 0 ${buildingScale * 3}vmin,
         #4de 0 ${buildingScale * 4}vmin,
-        #104 0 ${buildingScale * 5}vmin,
+        #215 0 ${buildingScale * 5}vmin,
         #4de 0 ${buildingScale * 6}vmin,
-        #104 0 ${buildingScale * 7}vmin,
+        #215 0 ${buildingScale * 7}vmin,
         #4de 0 ${buildingScale * 8}vmin,
-        #104 0 ${buildingScale * 9}vmin,
+        #215 0 ${buildingScale * 9}vmin,
         #56f 0 ${buildingScale * 10}vmin,
-        #104 0 ${buildingScale * 11}vmin,
+        #215 0 ${buildingScale * 11}vmin,
         #56f 0 ${buildingScale * 12}vmin,
-        #104 0 ${buildingScale * 13}vmin,
+        #215 0 ${buildingScale * 13}vmin,
         #b2f 0 ${buildingScale * 14}vmin,
-        #104 0 ${buildingScale * 15}vmin,
-        #b2f 0 ${buildingScale * 16}vmin,
-        #104 0 ${buildingScale * 18}vmin,
-        #b2f 0 ${buildingScale * 19}vmin,
-        #104 0 ${buildingScale * 20}vmin,
+        #215 0 ${buildingScale * 15}vmin,
+
+        #215 0 ${buildingScale * 20}vmin,
         #b2f 0 ${buildingScale * 21}vmin,
-        #104 0 ${buildingScale * 22}vmin,
+        #215 0 ${buildingScale * 22}vmin,
         #56f 0 ${buildingScale * 23}vmin,
-        #104 0 ${buildingScale * 24}vmin,
+        #215 0 ${buildingScale * 24}vmin,
         #56f 0 ${buildingScale * 25}vmin,
-        #104 0 ${buildingScale * 26}vmin,
+        #215 0 ${buildingScale * 26}vmin,
         #4de 0 ${buildingScale * 27}vmin,
-        #104 0 ${buildingScale * 28}vmin,
+        #215 0 ${buildingScale * 28}vmin,
         #4de 0 ${buildingScale * 29}vmin,
-        #104 0 ${buildingScale * 30}vmin,
+        #215 0 ${buildingScale * 30}vmin,
         #4de 0 ${buildingScale * 31}vmin,
-        #104 0 ${buildingScale * 32}vmin,
+        #215 0 ${buildingScale * 32}vmin,
         #4de 0 ${buildingScale * 33}vmin,
-        #104 0
+        #215 0
       )
     `;
     this.building1.sides[3].style.mask = 'linear-gradient(#000 50%, #0000)';
